13fb462365679481ebb7d1fe7d1b191d0cdbf15b
John Levon <john.levon@joyent.com>
2018-09-28 17:18:54 +0000

    OS-7270 ctfconvert doesn't handle DW_AT_specification

--- usr/src/lib/libctf/common/ctf_dwarf.c
+++ usr/src/lib/libctf/common/ctf_dwarf.c
@@ -469,7 +469,7 @@ ctf_dwarf_refdie(ctf_die_t *cdp, Dwarf_Die die, Dwarf_Half name,
 	Dwarf_Off off;
 	Dwarf_Error derr;
 
-	if ((ret = ctf_dwarf_ref(cdp, die, DW_AT_type, &off)) != 0)
+	if ((ret = ctf_dwarf_ref(cdp, die, name, &off)) != 0)
 		return (ret);
 
 	off += cdp->cd_cuoff;
@@ -628,6 +628,17 @@ ctf_dwarf_offset(ctf_die_t *cdp, Dwarf_Die die, Dwarf_Off *offsetp)
 	    "failed to get die offset: %s\n",
 	    dwarf_errmsg(derr));
 	return (ECTF_CONVBKERR);
+}
+
+/* simpler variant for debugging output */
+static Dwarf_Off
+ctf_die_offset(Dwarf_Die die)
+{
+	Dwarf_Off off = -1;
+	Dwarf_Error derr;
+
+	(void) dwarf_dieoffset(die, &off, &derr);
+	return (off);
 }
 
 static int
@@ -1995,11 +2006,31 @@ ctf_dwarf_convert_variable(ctf_die_t *cdp, Dwarf_Die die)
 	ctf_id_t id;
 	ctf_dwvar_t *cdv;
 
-	if ((ret = ctf_dwarf_boolean(cdp, die, DW_AT_declaration, &b)) != 0) {
-		if (ret != ENOENT)
+	/* Skip "Non-Defining Declarations" */
+	if ((ret = ctf_dwarf_boolean(cdp, die, DW_AT_declaration, &b)) == 0) {
+		if (b != 0)
+			return (0);
+	} else if (ret != ENOENT) {
+		return (ret);
+	}
+
+	/*
+	 * If we find a DIE of "Declarations Completing Non-Defining
+	 * Declarations", we will use the referenced type's DIE.  This isn't
+	 * quite correct, e.g. DW_AT_decl_line will be the forward declaration
+	 * not this site.  It's sufficient for what we need, however: in
+	 * particular, we should find DW_AT_external as needed there.
+	 */
+	if ((ret = ctf_dwarf_refdie(cdp, die,
+	    DW_AT_specification, &tdie)) == 0) {
+		Dwarf_Off offset;
+		if ((ret = ctf_dwarf_offset(cdp, tdie, &offset)) != 0)
 			return (ret);
-	} else if (b != 0) {
-		return (0);
+		ctf_dprintf("die 0x%llx DW_AT_specification -> die 0x%llx\n",
+		    ctf_die_offset(die), ctf_die_offset(tdie));
+		die = tdie;
+	} else if (ret != ENOENT) {
+		return (ret);
 	}
 
 	if ((ret = ctf_dwarf_string(cdp, die, DW_AT_name, &name)) != 0 &&
