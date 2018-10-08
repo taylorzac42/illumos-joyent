alarcher/illumos-gate

pnglite: move pnglite to usr/src/common

usr/src/common/pnglite/pnglite.c
@@ -3,8 +3,16 @@
  * For conditions of distribution and use, see copyright notice in pnglite.h
  */
 
+#ifdef STAND
 #include <sys/cdefs.h>
 #include <stand.h>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
 #include <zlib.h>
 #include "pnglite.h"
 

taylorzac42/illumos-joyent

usr/src/common/pnglite/pnglite.c
@@ -3,8 +3,16 @@
  * For conditions of distribution and use, see copyright notice in pnglite.h
  */
 
+#ifdef _STANDALONE
 #include <sys/cdefs.h>
 #include <stand.h>
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#endif
 #include <zlib.h>
 #include "pnglite.h"
 
@@ -192,7 +200,7 @@ done:
 		png->fd = -1;
 		return (result);
 	}
-	
+
 	return (result);
 }
 